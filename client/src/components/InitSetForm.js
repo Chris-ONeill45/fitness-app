e647f6c99a3509f039e3a57ee4e974f3d36c1419
Chris O'Neill <chris_oslow@hotmail.com>
2024-03-30 09:45:10 +0000

    frontend and backend file restructure

--- client/src/components/InitSetForm.js
+++ client/src/components/InitSetForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const InitSetForm = () => {
-  return <div className="init-set-form"></div>;
+  return <div className="init-set-form">initsetfrom</div>;
 };
 
 export default InitSetForm;
