9a60012c5e9f3bada9e0cf6f0ad2ade4b74d5b80
James Carrier <jic191278@gmail.com>
2024-03-31 10:44:15 +0100

    add firebase login auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '../styles/app.css';
+import NavBar from './NavBar';
 import LoginForm from './LogInForm';
 import RegistrationForm from './RegistrationForm';
+import Footer from './Footer';
 
 const App = () => {
+  const [userStatus, setUserStatus] = useState({
+    signedin: false,
+    signedinEmail: null,
+    message: 'Not signed in',
+  });
+
   return (
     <div className="app">
       <Router>
+        <NavBar />
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route
+            path="/"
+            element={
+              <LoginForm
+                userStatus={userStatus}
+                setUserStatus={setUserStatus}
+              />
+            }
+          />
           <Route path="/registration" element={<RegistrationForm />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
